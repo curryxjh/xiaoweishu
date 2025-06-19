b8f594229498d691e8eae4e044b2467d94d29cf9
xjh <nil541203@gmail.com>
2025-06-19 21:53:46 +0800

    fe

--- internal/web/user.go
+++ internal/web/user.go
@@ -1,21 +1,83 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	regexp "github.com/dlclark/regexp2"
+)
 
 // UserHandler 定义所有和user相关的路由
-type UserHandler struct{}
-
-func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
-	server.POST("/users/signup", u.SignUp)
-
-	server.POST("/users/signin", u.LogIn)
-
-	server.POST("/users/edit", u.Edit)
-
-	server.GET("/users/profile", u.Profile)
+type UserHandler struct {
+	emailExp    *regexp.Regexp
+	passwordExp *regexp.Regexp
 }
 
-func (u *UserHandler) SignUp(c *gin.Context) {}
+func NewUserHandler() *UserHandler {
+	const (
+		emailRegexPattern    = `^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$`
+		passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
+	)
+
+	return &UserHandler{
+		emailExp:    regexp.MustCompile(emailRegexPattern, regexp.None),
+		passwordExp: regexp.MustCompile(passwordRegexPattern, regexp.None),
+	}
+}
+
+func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
+	ug := server.Group("/users")
+	ug.POST("/signup", u.SignUp)
+	ug.POST("/signin", u.LogIn)
+	ug.POST("/edit", u.Edit)
+	ug.GET("/profile", u.Profile)
+}
+
+func (u *UserHandler) SignUp(c *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Password        string `json:"password"`
+	}
+
+	var req SignUpReq
+	// Bind 方法会根据 Content-Type 来解析你的数据到 req 里面
+	// 解析错了，就会返回一个 400 的错误
+	if err := c.Bind(&req); err != nil {
+		log.Println(err)
+		return
+	}
+
+	ok, err := u.emailExp.MatchString(req.Email)
+	if err != nil {
+		c.String(http.StatusOK, "系统错误")
+		log.Println(err)
+		return
+	}
+
+	if !ok {
+		c.String(http.StatusOK, "你的邮箱格式不正确")
+		return
+	}
+
+	ok, err = u.passwordExp.MatchString(req.Password)
+	if err != nil {
+		c.String(http.StatusOK, "系统错误")
+		log.Println(err)
+		return
+	}
+
+	if req.ConfirmPassword != req.ConfirmPassword {
+		c.String(http.StatusOK, "两次输入的密码不一致")
+		return
+	}
+
+	if !ok {
+		c.String(http.StatusOK, "密码必须大于8位，且包含特殊字符")
+		return
+	}
+	c.String(http.StatusOK, "success")
+}
 
 func (u *UserHandler) LogIn(c *gin.Context) {}
 
--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 	"xiaoweishu/internal/web"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	server := gin.Default()
-	u := web.UserHandler{}
+
+	server.Use(cors.New(cors.Config{
+		//AllowOrigins: []string{"http://localhost:3000"},
+		//AllowMethods: []string{},
+		AllowHeaders: []string{"authorization", "Content-Type"},
+		//ExposeHeaders: []string{"x-jwt-token"},
+		// 是否允许携带用户认证信息，如cookie
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") || strings.HasPrefix(origin, "https://") {
+				return true
+			}
+			return strings.Contains(origin, "http://youcompany.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
 	server.Run(":8080")
 }
