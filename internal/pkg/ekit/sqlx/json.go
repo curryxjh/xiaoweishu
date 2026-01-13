1a0f22d7343d28d23b9d8ee9fed76de394a092aa
curryxjh <nil541203@gmail.com>
2026-01-13 16:21:50 +0800

    feat: 完成JsonColumn设计

--- internal/pkg/ekit/sqlx/json.go
+++ internal/pkg/ekit/sqlx/json.go
@@ -3,8 +3,13 @@ package sqlx
 import (
 	"database/sql/driver"
 	"encoding/json"
-	"errors"
+	"fmt"
 )
+
+// JsonColumn 核心目的是在 go 程序与数据库(MySQL PostgreSQL SQLite)之间建立一个自动化的 JSON 转换机制
+// 日常开发中, 我们经常需要把复杂的对象存储到数据库的一个字段中
+// 手动处理时, 一般情况下我们需要在存储之前调用 json.Marshal(), 在读取之后调用 json.Unmarshal()
+// 数据库驱动只认识基本数据类型(string, int, []byte)
 
 // JsonColumn 代表存储字段的 JSON类型
 // 主要用于没有提供默认 JSON 类型的数据库
@@ -38,7 +43,7 @@ func (j *JsonColumn[T]) Scan(src any) error {
 	case string:
 		bs = []byte(val)
 	default:
-		return errors.New("invalid type")
+		return fmt.Errorf("ekit: json column can only scan string or []byte, but got %T", val)
 	}
 	if err := json.Unmarshal(bs, &j.Val); err != nil {
 		return err
